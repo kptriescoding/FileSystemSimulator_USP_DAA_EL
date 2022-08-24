920305a6e90a263837c4458903560a3f759856e3
kptriescoding <karthikpai0*@gmail.com>
2022-08-24 21:01:35 +0530

    SQL Added

--- src/main/java/Database/Sql_test.java
+++ src/main/java/Database/Sql_test.java
@@ -1,42 +1,31 @@
 package Database;
 
-import java.sql.*;  // Using 'Connection', 'Statement' and 'ResultSet' classes in java.sql package
+import java.sql.*;
 
-public class Sql_test {   // Save as "JdbcSelectTest.java"
+public class Sql_test {
     public static void main(String[] args) {
         try (
                 Connection conn = DriverManager.getConnection(
                         "jdbc:mysql://localhost:3306/ebookshop?allowPublicKeyRetrieval=true&useSSL=false&serverTimezone=UTC",
-                        "kptries@localhost", "password");
-                // The format is: "jdbc:mysql://hostname:port/databaseName", "username", "password"
-
-                // Step 2: Construct a 'Statement' object called 'stmt' inside the Connection created
+                        "kptries", "password");
                 Statement stmt = conn.createStatement();
         ) {
-            // Step 3: Write a SQL query string. Execute the SQL query via the 'Statement'.
-            //  The query result is returned in a 'ResultSet' object called 'rset'.
             String strSelect = "select title, price, qty from books";
-            System.out.println("The SQL statement is: " + strSelect + "\n"); // Echo For debugging
+            System.out.println("The SQL statement is: " + strSelect + "\n");
 
             ResultSet rset = stmt.executeQuery(strSelect);
-
-            // Step 4: Process the 'ResultSet' by scrolling the cursor forward via next().
-            //  For each row, retrieve the contents of the cells with getXxx(columnName).
             System.out.println("The records selected are:");
             int rowCount = 0;
-            // Row-cursor initially positioned before the first row of the 'ResultSet'.
-            // rset.next() inside the whole-loop repeatedly moves the cursor to the next row.
-            // It returns false if no more rows.
-            while(rset.next()) {   // Repeatedly process each row
-                String title = rset.getString("title");  // retrieve a 'String'-cell in the row
-                double price = rset.getDouble("price");  // retrieve a 'double'-cell in the row
-                int    qty   = rset.getInt("qty");       // retrieve a 'int'-cell in the row
+            while (rset.next()) {
+                String title = rset.getString("title");
+                double price = rset.getDouble("price");
+                int qty = rset.getInt("qty");
                 System.out.println(title + ", " + price + ", " + qty);
                 ++rowCount;
             }
             System.out.println("Total number of records = " + rowCount);
 
-        } catch(SQLException ex) {
+        } catch (SQLException ex) {
             System.out.println("Here");
             ex.printStackTrace();
         }
