f7c4f49dd9015f968982c8adf2b3d6164721b78c
kptriescoding <karthikpai0*@gmail.com>
2022-08-25 10:52:43 +0530

    Dir Update

--- src/main/java/FileSystem/SuperNode.java
+++ src/main/java/FileSystem/SuperNode.java
@@ -1,12 +1,25 @@
 package FileSystem;
 
 import Database.SqlCommands;
+import Models.Directory;
 
 public class SuperNode {
     public SuperNode(){
         SqlCommands sql=new SqlCommands();
         SuperNode superNode=(SuperNode) sql.retrieveObject(1);
-
+        if(superNode==null){
+            this.currentNode=2;
+            this.numberofNodes=1;
+            Directory root= new Directory(this,1);
+            this.numberofNodes++;
+            sql.storeObject(this);
+            sql.storeObject(root);
+        }
+    }
+    public void updateSuperNode(){
+        SqlCommands sql=new SqlCommands();
+        numberofNodes=numberofNodes+1;
+        sql.UpdateObject(this,1);
     }
     private int currentNode;
     private int numberofNodes;
