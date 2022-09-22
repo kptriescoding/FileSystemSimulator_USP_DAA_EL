c2dae8aaa776583c957f619cf135eb605409fcf8
dileep-shar <dilsharma0220@gmail.com>
2022-09-23 02:35:37 +0530

    gedit+some minor change

--- src/main/java/JavaFxCustomClasses/TextEditor.java
+++ src/main/java/JavaFxCustomClasses/TextEditor.java
@@ -3,28 +3,62 @@ package JavaFxCustomClasses;
 import Database.SqlCommands;
 import Models.File;
 import com.example.file_system_simulator.FileSystem;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.NodeOrientation;
+import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
-import javafx.scene.layout.AnchorPane;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.*;
 import FileSystem.*;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 
-public class TextEditor extends AnchorPane {
+public class TextEditor extends VBox {
     private int fileInodeNumber;
     private String fileName;
-    public TextEditor(int fileInodeNumber, String fileName){
-        this.fileInodeNumber=fileInodeNumber;
-        this.fileName=fileName;
-        Button saveButton=new Button("Save");
-        SqlCommands sql =new SqlCommands();
-        INode inode=(INode)sql.retrieveObject(fileInodeNumber);
-        File f=(File)inode.getFileReference();
-        TextArea textArea=new TextArea();
+
+    public TextEditor(int fileInodeNumber, String fileName) {
+        this.fileInodeNumber = fileInodeNumber;
+        this.fileName = fileName;
+
+
+        SqlCommands sql = new SqlCommands();
+        INode inode = (INode) sql.retrieveObject(fileInodeNumber);
+        File f = (File) inode.getFileReference();
+        HBox hBox = new HBox();
+        hBox.setStyle("-fx-background-color: #3e3e3e;");
+        hBox.setMinHeight(35);
+        hBox.setNodeOrientation(NodeOrientation.RIGHT_TO_LEFT);
+
+        Button saveButton = new Button("Save");
+        saveButton.setStyle("-fx-background-color: #5d5d5d;");
+        saveButton.setPadding(new Insets(5, 9, 5, 9));
+        hBox.getChildren().add(saveButton);
+        saveButton.setFont(Font.font("ubuntu", FontWeight.BOLD, 15));
+        saveButton.setTextFill(Color.rgb(255,255,255));
+        HBox.setMargin(saveButton, new Insets(4, 3, 4, 30));
+        hBox.alignmentProperty().set(Pos.CENTER_LEFT);
+        saveButton.setOnMousePressed(event -> {saveButton.setStyle("-fx-background-color: #2f2f2f;");});
+        saveButton.setOnMouseReleased(event -> {saveButton.setStyle("-fx-background-color: #5d5d5d;");});
+
+        TextArea textArea = new TextArea();
+        textArea.setFont(Font.font("ubuntu", FontWeight.BOLD, 14));
+        ;
+//        textArea.setBackground(Background.fill(Color.rgb(0,27,51)));
+        textArea.setStyle("-fx-control-inner-background:#001b33; -fx-background-color: #001b33;");
+
         textArea.setText(f.getContents());
-        saveButton.setOnAction((ae)->{
-            FileSystem.commands.saveTextFile(fileInodeNumber,textArea.getText());
+        saveButton.setOnAction((ae) -> {
+            FileSystem.commands.saveTextFile(fileInodeNumber, textArea.getText());
         });
         textArea.setPrefColumnCount(20);
         textArea.setPrefRowCount(20);
-        this.getChildren().addAll(textArea,saveButton);
+        VBox.setVgrow(textArea, Priority.ALWAYS);
+        this.setBackground(Background.fill(Color.rgb(0, 27, 51)));
+        this.getChildren().addAll(hBox, textArea);
     }
 }
--- src/main/java/com/example/file_system_simulator/GuiController.java
+++ src/main/java/com/example/file_system_simulator/GuiController.java
@@ -9,11 +9,16 @@ import JavaFxCustomClasses.FileIcon;
 import Models.DirContents;
 import Models.Directory;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 
 import java.util.Objects;
 
 public class GuiController {
+    public Button back;
+    public HBox current_directory_path;
     @FXML
     private GridPane gridPane;
     public void updateGui(){
@@ -41,4 +46,22 @@ public class GuiController {
         }
     }
 }
+
+    public void backPressestarted(MouseEvent mouseEvent) {
+        back.setStyle("-fx-background-color: #000000;");
+    }
+    public void backPressedHover(MouseEvent mouseEvent) {
+        back.setStyle("-fx-background-color: #2f2f2f;");
+
+    }
+    public void backPressed(MouseEvent mouseEvent) {
+        back.setStyle("-fx-background-color: #2f2f2f;");
+
+//        "-fx-border-color: #000000; -fx-border-width: 0.3; -fx-background-color: #424648"
+//TODO go to previous directory if possible and fetch the path till now one by one and send it
+
+
+    }
+
+
 }
