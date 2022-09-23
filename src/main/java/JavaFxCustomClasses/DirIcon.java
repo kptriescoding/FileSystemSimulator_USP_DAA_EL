e9654f6f3086108cac015547c7cf240b9b108ddc
dileep-shar <dilsharma0220@gmail.com>
2022-09-24 01:35:53 +0530

    some

--- src/main/java/JavaFxCustomClasses/DirIcon.java
+++ src/main/java/JavaFxCustomClasses/DirIcon.java
@@ -4,13 +4,20 @@ import Database.SqlCommands;
 import FileSystem.INode;
 import Models.Directory;
 import com.example.file_system_simulator.TerminalController;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.ContentDisplay;
+import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.*;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.VBox;
 
 import java.util.Objects;
 
-public class DirIcon extends Button {
+public class DirIcon extends VBox {
 
     private final String STYLE_NORMAL = "-fx-background-color: transparent; -fx-padding: 2, 2, 2, 2;";
     private final String STYLE_PRESSED = "-fx-background-color: transparent; -fx-padding: 3 1 1 3;";
@@ -18,46 +25,90 @@ public class DirIcon extends Button {
     private INode parINode;
     private int inodeNumber;
 
-    public DirIcon(INode parInode, String name, int inodeNumber,boolean setbit) {
-        super(name);
+    public DirIcon(INode parInode, String name, int inodeNumber, boolean setbit) {
+
         this.inodeNumber = inodeNumber;
         this.parINode = parInode;
+        this.setMinHeight(80);
+        this.setMaxWidth(80);
+        ImageView icon = new ImageView("https://previews.123rf.com/images/jovanas/jovanas1810/jovanas181001136/110959550-file-icon-folder-icon.jpg");
+        icon.setPreserveRatio(true);
+        icon.pickOnBoundsProperty().set(true);
+        icon.setFitHeight(80);
+        icon.setFitWidth(80);
+
+        DirectoryRightClickOptions directoryRightClickOptions = new DirectoryRightClickOptions(name, inodeNumber);
 
 
 
-
-        ImageView image = new ImageView("https://previews.123rf.com/images/jovanas/jovanas1810/jovanas181001136/110959550-file-icon-folder-icon.jpg");
-        image.setFitHeight(100);
-        image.setPreserveRatio(true);
-        setGraphic(image);
+//
+//        ImageView image = new ImageView();
+//        image.setFitHeight(100);
+//        image.setPreserveRatio(true);
+//        setGraphic(image);
         setStyle(STYLE_NORMAL);
-        this.setContextMenu(new DirectoryRightClickOptions(name, inodeNumber));
-        setOnMousePressed(event -> setStyle(STYLE_PRESSED));
-        setOnMouseReleased(event -> setStyle(STYLE_NORMAL));
-        setContentDisplay(ContentDisplay.TOP);
-        setOnAction((ae) -> {
-            TerminalController.guiCommands("cd " + name);
-            INode inode = parInode;
-            Directory d = (Directory) inode.getFileReference();
 
-            StringBuilder sb = new StringBuilder();
-            while (setbit) {
-                try {
-                    if (d.getName().equals("/")) {
-                        sb.append("/");
-                       break;
+//        this.getChildren().get(0).setContextMenu(new DirectoryRightClickOptions(name, inodeNumber));
+//
+//        setOnMousePressed(event -> setStyle(STYLE_PRESSED));
+//        setOnMouseReleased(event -> setStyle(STYLE_NORMAL));
+
+
+        TextField editName = new TextField();
+//        editName.disableProperty().set(true);
+        editName.setAlignment(Pos.CENTER);
+        editName.setStyle("-fx-background-color: transparent; -fx-border-color: transparent;");
+        editName.setText(name);
+
+
+        this.getChildren().addAll(icon, editName);
+        VBox.setVgrow(icon, Priority.ALWAYS);
+
+        this.getChildren().get(1).setOnKeyPressed(event -> {
+            if (event.getCode().equals(KeyCode.ENTER)) {
+
+                editName.setText(editName.getText());
+                editName.disableProperty().set(true);
+            }
+        });
+
+
+//        setContentDisplay(ContentDisplay.TOP);
+        this.getChildren().get(0).setOnMouseClicked(event -> {
+            if (event.getClickCount() == 2) {
+                TerminalController.guiCommands("cd " + name);
+                INode inode = parInode;
+                Directory d = (Directory) inode.getFileReference();
+
+                StringBuilder sb = new StringBuilder();
+                while (setbit) {
+                    try {
+                        if (d.getName().equals("/")) {
+                            sb.append("/");
+                            break;
+                        }
+
+                        inode = (INode) sql.retrieveObject(d.getContents().get(1).getInodeNumber());
+                        d = (Directory) inode.getFileReference();
+                        sb.append(d.getName());
+                        if (!Objects.equals(d.getName(), "")) sb.append("/");
+                    } catch (Exception e) {
+                        break;
                     }
-
-                    inode = (INode) sql.retrieveObject(d.getContents().get(1).getInodeNumber());
-                    d = (Directory) inode.getFileReference();
-                    sb.append(d.getName());
-                    if(!Objects.equals(d.getName(), "")) sb.append("/");
-                } catch (Exception e) {
-                    break;
+                }
+                System.out.println(sb.reverse().toString());
+            }
+        });
+        this.getChildren().get(0).setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                if (event.getButton().equals(MouseButton.SECONDARY)) {
+                    System.out.println("happening");
+                    directoryRightClickOptions.show(icon, event.getScreenX(), event.getScreenY());
                 }
             }
-            System.out.println(sb.reverse().toString());
         });
+
     }
 
 }
--- src/main/java/JavaFxCustomClasses/FileIcon.java
+++ src/main/java/JavaFxCustomClasses/FileIcon.java
@@ -2,9 +2,13 @@ package JavaFxCustomClasses;
 
 import FileSystem.INode;
 import com.example.file_system_simulator.FileSystem;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.control.Button;
 import javafx.scene.control.ContentDisplay;
+import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 
 public class FileIcon extends Button {
 
@@ -12,8 +16,20 @@ public class FileIcon extends Button {
     private final String STYLE_PRESSED = "-fx-background-color: transparent; -fx-padding: 3 1 1 3;";
     private INode parINode;
     private int inodeNumber;
+    TextField tf = new TextField();
+
     public FileIcon(INode parINode, String name, int inodeNumber) {
         super(name);
+        setText(name);
+        setOnMouseClicked(e -> {
+
+        });
+        tf.setOnAction(ae -> {
+//              if (validateText(tf.getText())) {// this is where you would validate the text
+            setText(tf.getText());
+//            setGraphic(null);
+//            }
+        });
         this.parINode=parINode;
         this.inodeNumber=inodeNumber;
         ImageView image = new ImageView("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTVENxI54U9ljaiM6bCAEWA4iq072kE5K4H9IU2Ach4uQ&s");
@@ -24,8 +40,19 @@ public class FileIcon extends Button {
         setOnMousePressed(event -> setStyle(STYLE_PRESSED));
         setOnMouseReleased(event -> setStyle(STYLE_NORMAL));
         setContentDisplay(ContentDisplay.TOP);
-        this.setOnAction((ae)->{
-            FileSystem.newTextEditor(inodeNumber,name);
+//        this.setOnAction((ae)->{
+//
+//
+//        });
+        this.setOnMouseClicked(event -> {
+            if(event.getClickCount()==2){
+                FileSystem.newTextEditor(inodeNumber,name);
+            }
+            else {
+                tf.setText(getText());
+                setText("");
+                setGraphic(tf);
+            }
         });
         this.setContextMenu(new FileRightClickOptions(name, inodeNumber));
     }
